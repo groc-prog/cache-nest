d5f66eaacc7cc831c0486abe8f3b8baffd8e9577
groc-prog <marc.troisner@gmail.com>
2024-09-11 10:53:59 +0200

    feat(api): set up basic elysia server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello via bun');
+import { Elysia } from 'elysia';
+
+import { getApiConfiguration } from '@/setup';
+import logger from '@/utils/logger';
+
+const configuration = await getApiConfiguration();
+
+logger.info('Starting server');
+new Elysia()
+  .get('/', () => ({ msg: 'Hello Elysia' }))
+  .listen(
+    {
+      port: configuration.server.port,
+      hostname: configuration.server.host,
+    },
+    (bunServer) => {
+      logger.info(`Server started on ${bunServer.hostname}:${bunServer.port}`);
+    },
+  );
