ca56fb1ed9faac0953fb3f2e5e04df95f449c1c8
groc-prog <marc.troisner@gmail.com>
2024-09-11 11:16:41 +0200

    feat(api): add elysia opentelemetry plugin

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,27 @@
+import { opentelemetry } from '@elysiajs/opentelemetry';
 import { Elysia } from 'elysia';
 
 import { getApiConfiguration } from '@/setup';
 import logger from '@/utils/logger';
 
+import { tracer } from './utils/opentelemetry';
+
 const configuration = await getApiConfiguration();
 
 logger.info('Starting server');
 new Elysia()
-  .get('/', () => ({ msg: 'Hello Elysia' }))
+  .use(opentelemetry())
+  .get('/', async () => {
+    await tracer.startActiveSpan('test', async (span) => {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+      });
+
+      span.end();
+    });
+
+    return { msg: 'Hello Elysia' };
+  })
   .listen(
     {
       port: configuration.server.port,
