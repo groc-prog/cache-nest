a24602b0905107522d462bab4b83879c5038c0c4
groc-prog <marc.troisner@gmail.com>
2024-09-26 14:21:44 +0200

    chore: remove vitest as global dep and update config for web

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     watch: false,
+    include: ['test/**/*.{spec,test}.ts'],
     coverage: {
       enabled: true,
       provider: 'v8',
-      include: ['**/*/src/**'],
+      include: ['src/**'],
     },
   },
 });
